57395b22a2b39c81a3bcc8edbe8abeb03f370637
Edward Chen <yoyokko@gmail.com>
2015-01-22 11:37:02 +0800

    initial commit
    convert clang-format to astyle

---
+++ AStyleFormat/TRVSCodeFragment.h
@@ -0,0 +1,36 @@
+//
+//  TRVSCodeFragment.h
+//  ClangFormat
+//
+//  Created by Travis Jeffery on 1/9/14.
+//  Copyright (c) 2014 Travis Jeffery. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class TRVSCodeFragment;
+
+@interface TRVSCodeFragmentBuilder : NSObject
+
+- (TRVSCodeFragment *) build;
+
+@property (nonatomic, copy) NSString *string;
+@property (nonatomic, copy) NSString *formattedString;
+@property (nonatomic) NSRange range;
+@property (nonatomic, strong) NSURL *fileURL;
+
+@end
+
+@interface TRVSCodeFragment : NSObject
+
++ (instancetype) fragmentUsingBlock:(void (^)(TRVSCodeFragmentBuilder *builder)) block;
+
+- (void) formatWithStyle:(NSString *) style usingClangFormatAtLaunchPath:(NSString *) launchPath block:(void (^)(NSString *formattedString, NSError *error)) block;
+
+@property (nonatomic, copy) NSString *string;
+@property (nonatomic, copy) NSString *formattedString;
+@property (nonatomic) NSRange range;
+@property (nonatomic, strong) NSURL *fileURL;
+@property (nonatomic, strong) NSError *error;
+
+@end
---
+++ AStyleFormat/TRVSCodeFragment.m
@@ -0,0 +1,96 @@
+//
+//  TRVSCodeFragment.m
+//  ClangFormat
+//
+//  Created by Travis Jeffery on 1/9/14.
+//  Copyright (c) 2014 Travis Jeffery. All rights reserved.
+//
+
+#import "TRVSCodeFragment.h"
+
+@interface TRVSCodeFragment()
+
+- (instancetype) initWithBuilder:(TRVSCodeFragmentBuilder *) builder;
+
+@end
+
+@implementation TRVSCodeFragmentBuilder
+
+- (TRVSCodeFragment *) build
+{
+    return [[TRVSCodeFragment alloc] initWithBuilder:self];
+}
+
+@end
+
+@implementation TRVSCodeFragment
+
++ (instancetype) fragmentUsingBlock:
+
+(void (^)(TRVSCodeFragmentBuilder *builder)) block
+{
+    TRVSCodeFragmentBuilder *builder = [TRVSCodeFragmentBuilder new];
+    block(builder);
+    return [builder build];
+}
+
+- (instancetype) initWithBuilder:(TRVSCodeFragmentBuilder *) builder
+{
+    if (self = [super init])
+    {
+        _string = [builder.string copy];
+        _range = builder.range;
+        _fileURL = builder.fileURL;
+    }
+    return self;
+}
+
+- (void) formatWithStyle:(NSString *) style
+usingClangFormatAtLaunchPath:(NSString *) launchPath
+                   block:(void (^)(NSString *formattedString,
+                                   NSError *error)) block
+{
+    NSURL *tmpFileURL = [self.fileURL URLByAppendingPathExtension:@"trvs"];
+    [self.string writeToURL:tmpFileURL
+                 atomically:YES
+                   encoding:NSUTF8StringEncoding
+                      error:NULL];
+    
+    NSPipe *outputPipe = [NSPipe pipe];
+    NSPipe *errorPipe = [NSPipe pipe];
+    
+    NSTask *task = [[NSTask alloc] init];
+    task.standardOutput = outputPipe;
+    task.standardError = errorPipe;
+    task.launchPath = launchPath;
+    task.arguments = @[
+                       [NSString stringWithFormat:@"--style=%@", style],
+                       @"-i",
+                       [tmpFileURL path]
+                       ];
+    
+    [outputPipe.fileHandleForReading readToEndOfFileInBackgroundAndNotify];
+    
+    [task launch];
+    [task waitUntilExit];
+    
+    NSData *errorData = [errorPipe.fileHandleForReading readDataToEndOfFile];
+    
+    self.formattedString = [NSString stringWithContentsOfURL:tmpFileURL
+                                                    encoding:NSUTF8StringEncoding
+                                                       error:NULL];
+    
+    block(self.formattedString,
+          errorData.length > 0
+          ? [NSError errorWithDomain:@"com.travisjeffery.error"
+                                code: -99
+                            userInfo:@
+             {
+             NSLocalizedDescriptionKey:[[NSString alloc] initWithData:errorData encoding:NSUTF8StringEncoding]
+             }]
+          : nil);
+    
+    [[NSFileManager defaultManager] removeItemAtURL:tmpFileURL error:NULL];
+}
+
+@end
